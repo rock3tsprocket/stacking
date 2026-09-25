70498345fbf05e32f8f792c1260d71dc343451a3
rock3tsprocket <rock-pie@hotmail.com>
2026-09-25 14:23:25 +0100

    [interpreter] switch to 4-space indentation

--- stacklan.c
+++ stacklan.c
@@ -5,102 +5,102 @@
 #define STACK_SIZE 30000
 
 int main(int argc, char *argv[]) {
-	/* Prepare variables */
-	uint8_t  stack[STACK_SIZE+1];
+    /* Prepare variables */
+    uint8_t  stack[STACK_SIZE+1];
     uint8_t  reg = 0;
-	uint16_t sp = 0;
-	int      ip = 0;
-	int      filesize = 0;
+    uint16_t sp = 0;
+    int      ip = 0;
+    int      filesize = 0;
 
     /* i */
     int i = 0;
-	
-	if (!argv[1]) {
-		fprintf(stderr, "No file specified.\n");
-		return 1;
-	}
+    
+    if (!argv[1]) {
+        fprintf(stderr, "No file specified.\n");
+        return 1;
+    }
 
-	/* Load file */
-	FILE *fileptr = fopen(argv[1], "r");
-	if (!fileptr) {
-		fprintf(stderr, "File does not exist\n");
-		return 1;
-	}
+    /* Load file */
+    FILE *fileptr = fopen(argv[1], "r");
+    if (!fileptr) {
+        fprintf(stderr, "File does not exist\n");
+        return 1;
+    }
 
-	fseek(fileptr, 0, SEEK_END);
-	filesize = ftell(fileptr);
-	fseek(fileptr, 0, SEEK_SET);
-	char code[filesize];
-	fread(code, filesize, 1, fileptr);
-	fclose(fileptr);
-	
-	/* Prepare jump table */
-	uint32_t jump_sp = 0;
-	uint32_t jump_stack[filesize];
-	uint32_t jump[filesize];
-	for (i = 0; i < filesize; i++) {
-		if (code[i] == '[') {
-			jump_stack[++jump_sp] = i;
-		}
-		else if (code[i] == ']') {
-			jump[i] = jump_stack[jump_sp--];
-			jump[jump[i]] = i;
-		}
-	}
+    fseek(fileptr, 0, SEEK_END);
+    filesize = ftell(fileptr);
+    fseek(fileptr, 0, SEEK_SET);
+    char code[filesize];
+    fread(code, filesize, 1, fileptr);
+    fclose(fileptr);
+    
+    /* Prepare jump table */
+    uint32_t jump_sp = 0;
+    uint32_t jump_stack[filesize];
+    uint32_t jump[filesize];
+    for (i = 0; i < filesize; i++) {
+        if (code[i] == '[') {
+            jump_stack[++jump_sp] = i;
+        }
+        else if (code[i] == ']') {
+            jump[i] = jump_stack[jump_sp--];
+            jump[jump[i]] = i;
+        }
+    }
 
-	/* Main loop */
-	while (ip < filesize) {
-		switch (code[ip]) {
-			case '>':
+    /* Main loop */
+    while (ip < filesize) {
+        switch (code[ip]) {
+            case '>':
                 if (sp+1 < STACK_SIZE)
                     stack[++sp] = 1;
-				break;
-			case '<':
+                break;
+            case '<':
                 if (sp > 0)
                     stack[sp--] = 0;
-				break;
-			case '+':
+                break;
+            case '+':
                 if (sp == 0) break;
                 stack[sp-1] += stack[sp];
                 stack[sp--] = 0;
-				break;
-			case '-':
+                break;
+            case '-':
                 if (sp < 1) break;
                 stack[sp-1] -= stack[sp];
                 stack[sp--] = 0;
-				break;
+                break;
             case '!':
                 stack[sp] = reg + stack[sp];
                 reg = stack[sp] - reg;
                 stack[sp] = stack[sp] - reg;
                 break;
-			case '.':
-				printf("%c", stack[sp]);
+            case '.':
+                printf("%c", stack[sp]);
                 fflush(stdout);
-				break;
-			case ',':
+                break;
+            case ',':
                 if (sp+1 < STACK_SIZE) --sp;
-    			fprintf(stderr, "\nInput (^D for EOF): ");
+                fprintf(stderr, "\nInput (^D for EOF): ");
                 fflush(stdout);
                 stack[++sp] = fgetc(stdin);
                 if ((int8_t)stack[sp] == EOF) stack[sp] = 0;
-				fprintf(stderr, "\n");
-				break;
-			case '[':
-				if (!stack[sp]) {
-					ip = jump[ip];
-				}
-				break;
-			case ']':
-				if (stack[sp]) {
-					ip = jump[ip];
-				}
-				break;
-		}
+                fprintf(stderr, "\n");
+                break;
+            case '[':
+                if (!stack[sp]) {
+                    ip = jump[ip];
+                }
+                break;
+            case ']':
+                if (stack[sp]) {
+                    ip = jump[ip];
+                }
+                break;
+        }
 
-		ip++;
-	}
+        ip++;
+    }
     puts("");
-	return 0;
+    return 0;
 }
 
